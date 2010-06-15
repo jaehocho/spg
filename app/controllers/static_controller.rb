cf2cfe3d0d1d035266ef2b7dfd95ab072a9d28a1
jaehocho <jaehocho@gmail.com>
2010-06-15 12:15:41 -0700

    made changes to the static page created the investment pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,12 +10,12 @@ class StaticController < ApplicationController
     end
 
     if !path || path.empty?
-      file = "/static/index.html.erb"
+      @file = "/static/index.html.erb"
     else
-      file = "/static/#{path.join("/")}.html.erb"
+      @file = "/static/#{path.join("/")}.html.erb"
     end
 
-    view_path = @template.view_paths[0].path + file
+    view_path = @template.view_paths[0].path + @file
     if !FileTest.exists?(view_path)
       respond_to do |format|
         format.html { render :file => "#{RAILS_ROOT}/public/404.html", :status => "404 Not Found" }
@@ -24,7 +24,7 @@ class StaticController < ApplicationController
     end
 
     respond_to do |format|
-      format.html { render :template => file  }
+      format.html { render :template => @file  }
     end
   end
 end
